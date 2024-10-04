96999f247636d5afea586883c0d1c65b274323c6
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-10-04 18:11:37 -0300

    fix: styled components config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  compiler: {
+    styledComponents: {
+      ssr: true,
+    },
+  }
+};
 
 export default nextConfig;
